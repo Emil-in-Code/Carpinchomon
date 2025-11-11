c091738176afb04f65f0ee6f810de96b844bdb7d
Emilio Flor <joanbernabe94@gmail.com>
2025-11-11 15:38:23 -0300

    se logró sacar el error object HTMLdivelement

--- jscarpinchomon/carpinchomon.js
+++ jscarpinchomon/carpinchomon.js
@@ -4,149 +4,175 @@ let vidasJugador = 3;
 let vidasRival = 3;
 
 function iniciarjuego() {
-    let sectionSeleccionarAtaque = document.getElementById("seleccionar-ataque")
-    sectionSeleccionarAtaque.style.display = "none"
+  let sectionSeleccionarAtaque = document.getElementById("seleccionar-ataque")
+  sectionSeleccionarAtaque.style.display = "none"
 
-    let sectionReiniciar = document.getElementById("reiniciar")
-    sectionReiniciar.style.display = "block"
+  let sectionReiniciar = document.getElementById("reiniciar")
+  sectionReiniciar.style.display = "none"
 
-    let botonCarpinchoJugador = document.getElementById("boton-carpincho")
-    botonCarpinchoJugador.addEventListener("click", seleccionCarpinchoJugador)
+  let ataquesDelJugador = document.getElementById("ataques-del-jugador")
+  ataquesDelJugador.style.display = "none"
 
-    let botonFuego = document.getElementById("boton-fuego")
-    botonFuego.addEventListener("click", ataqueFuego)
-    let botonAgua = document.getElementById("boton-agua")
-    botonAgua.addEventListener("click", ataqueAgua)
-    let botonTierra = document.getElementById("boton-tierra")
-    botonTierra.addEventListener("click", ataqueTierra) 
-    let botonReiniciar = document.getElementById("boton-reiniciar")
-    botonReiniciar.addEventListener("click", reiniciarJuego)
+  let ataquesDelEnemigo = document.getElementById("ataques-del-enemigo")
+  ataquesDelEnemigo.style.display = "none"
+
+  let botonCarpinchoJugador = document.getElementById("boton-carpincho")
+  botonCarpinchoJugador.addEventListener("click", seleccionCarpinchoJugador)
+
+  let botonFuego = document.getElementById("boton-fuego")
+  botonFuego.addEventListener("click", ataqueFuego)
+  let botonAgua = document.getElementById("boton-agua")
+  botonAgua.addEventListener("click", ataqueAgua)
+  let botonTierra = document.getElementById("boton-tierra")
+  botonTierra.addEventListener("click", ataqueTierra) 
+  let botonReiniciar = document.getElementById("boton-reiniciar")
+  botonReiniciar.addEventListener("click", reiniciarJuego)
 } 
 
 function seleccionCarpinchoJugador(){  
-    let sectionSeleccionarCarpincho = document.getElementById("seleccionar-carpincho")
-    sectionSeleccionarCarpincho.style.display = "none"
-    let sectionSeleccionarAtaque = document.getElementById("seleccionar-ataque")
-    sectionSeleccionarAtaque.style.display = "flex"
-    let inputNativus = document.getElementById("Nativus")
-    let inputNorcheto = document.getElementById("Norcheto")
-    let inputCarpi420 = document.getElementById("Carpi420")
-    let spanCarpinchoJugador = document.getElementById("carpincho-jugador")
-    
-    if (inputNativus.checked){
-        spanCarpinchoJugador.innerHTML = "Nativus"
-    } else if (inputNorcheto.checked){
-        spanCarpinchoJugador.innerHTML = "Norcheto"
-    } else if (inputCarpi420.checked){
-        spanCarpinchoJugador.innerHTML = "Carpi420"
-    } else {
-        alert("Debes seleccionar un carpincho para continuar")
-    }
+  let sectionSeleccionarCarpincho = document.getElementById("seleccionar-carpincho")
+  sectionSeleccionarCarpincho.style.display = "none"
 
-    let botonTierra = document.getElementById("boton-carpincho")
-    botonTierra.disabled = true
+  let sectionSeleccionarAtaque = document.getElementById("seleccionar-ataque")
+  sectionSeleccionarAtaque.style.display = "flex"
 
-    seleccionCarpinchoRival()
+  let ataquesDelJugador = document.getElementById("ataques-del-jugador")
+  ataquesDelJugador.style.display = "block"
+  
+  let ataquesDelEnemigo = document.getElementById("ataques-del-enemigo")
+  ataquesDelEnemigo.style.display = "block"
+
+  let inputNativus = document.getElementById("Nativus")
+  let inputNorcheto = document.getElementById("Norcheto")
+  let inputCarpi420 = document.getElementById("Carpi420")
+  let spanCarpinchoJugador = document.getElementById("carpincho-jugador")
+  
+  if (inputNativus.checked){
+      spanCarpinchoJugador.innerHTML = "Nativus"
+  } else if (inputNorcheto.checked){
+      spanCarpinchoJugador.innerHTML = "Norcheto"
+  } else if (inputCarpi420.checked){
+      spanCarpinchoJugador.innerHTML = "Carpi420"
+  } else {
+      alert ("Debes seleccionar un carpincho para continuar")
+    return;
+  }
+
+  seleccionCarpinchoRival()
 }
 
-function seleccionCarpinchoRival(){
-    let seleccionAleatoria = aleatorio (1,3)
-    let spanCarpinchoRival = document.getElementById("carpincho-rival")
 
-    if (seleccionAleatoria == 1){
-        spanCarpinchoRival.innerHTML = "Nativus"
-    } else if (seleccionAleatoria == 2){
-        spanCarpinchoRival.innerHTML = "Norcheto"
-    } else if (seleccionAleatoria == 3){
-        spanCarpinchoRival.innerHTML = "Carpi420"
-    }
+function seleccionCarpinchoRival(){
+  let seleccionAleatoria = aleatorio (1,3)
+  let spanCarpinchoRival = document.getElementById("carpincho-rival")
+
+  if (seleccionAleatoria == 1){
+      spanCarpinchoRival.innerHTML = "Nativus"
+  } else if (seleccionAleatoria == 2){
+      spanCarpinchoRival.innerHTML = "Norcheto"
+  } else{
+      spanCarpinchoRival.innerHTML = "Carpi420"
+  }
 }
 
 function ataqueFuego(){
-    ataqueJugador = "FUEGO"
-    ataqueAleatorioRival()
+  ataqueJugador = "FUEGO"
+  ataqueAleatorioRival()
 }
 function ataqueAgua(){
-    ataqueJugador = "AGUA"
-    ataqueAleatorioRival()
+  ataqueJugador = "AGUA"
+  ataqueAleatorioRival()
 }
 function ataqueTierra(){
-    ataqueJugador = "TIERRA"
-    ataqueAleatorioRival()
+  ataqueJugador = "TIERRA"
+  ataqueAleatorioRival()
 }
 
-
 function ataqueAleatorioRival(){
-    let ataqueAleatorio = aleatorio(1,3)
+  let ataqueAleatorio = aleatorio(1,3)
 
-    if (ataqueAleatorio == 1) {
-        ataqueRival = "FUEGO"
-    } else if (ataqueAleatorio == 2) {
-        ataqueRival = "AGUA"
-    } else if (ataqueAleatorio == 3) { 
-        ataqueRival = "TIERRA"
-    } 
-
-    combate()
-
+  if (ataqueAleatorio == 1) {
+      ataqueRival = "FUEGO"
+  } else if (ataqueAleatorio == 2) {
+      ataqueRival = "AGUA"
+  } else if (ataqueAleatorio == 3) { 
+      ataqueRival = "TIERRA"
+  } 
+  combate()
 } 
 
 function combate(){
-    let spanVidasJugador = document.getElementById("vidas-jugador")
-    let spanVidasRival =document.getElementById("vidas-rival")
+  let spanVidasJugador = document.getElementById("vidas-jugador")
+  let spanVidasRival = document.getElementById("vidas-rival")
 
-    if(ataqueRival == ataqueJugador) {
-        crearMensaje("EMPATE")
-    } else if(ataqueJugador == "FUEGO" && ataqueRival == "TIERRA"){
-        crearMensaje("GANASTE") 
-        vidasRival--
-        spanVidasRival.innerHTML = vidasRival
-    } else if(ataqueJugador == "AGUA" && ataqueRival == "FUEGO"){
-        crearMensaje("GANASTE")
-        vidasRival--
-        spanVidasRival.innerHTML = vidasRival
-    } else if(ataqueJugador == "TIERRA" && ataqueRival == "AGUA"){
-        crearMensaje("GANASTE")
-        vidasRival--
-        spanVidasRival.innerHTML = vidasRival
-    } else{
-        crearMensaje("PERDISTE")
-        vidasJugador--
-        spanVidasJugador.innerHTML = vidasJugador
-    } 
-    
-    revisarVidas()
+  if(ataqueRival == ataqueJugador) {
+      crearMensaje("EMPATE")
+  } else if(ataqueJugador == "FUEGO" && ataqueRival == "TIERRA"){
+      crearMensaje("GANASTE") 
+      vidasRival--
+      spanVidasRival.innerHTML = vidasRival
+  } else if(ataqueJugador == "AGUA" && ataqueRival == "FUEGO"){
+      crearMensaje("GANASTE")
+      vidasRival--
+      spanVidasRival.innerHTML = vidasRival
+  } else if(ataqueJugador == "TIERRA" && ataqueRival == "AGUA"){
+      crearMensaje("GANASTE")
+      vidasRival--
+      spanVidasRival.innerHTML = vidasRival
+  } else{
+      crearMensaje("PERDISTE")
+      vidasJugador--
+      spanVidasJugador.innerHTML = vidasJugador
+  }  
+  revisarVidas()
 }
 
 function revisarVidas(){
-    if(vidasRival == 0){
-      crearMensajefinal("¡Recuperaste tu territorio 😎!")
-    } else if (vidasJugador == 0){
-      crearMensajefinal("Lo lamento los chetos se quedaron con todo 😭")
-    }
+  if(vidasRival == 0){
+    crearMensajefinal("¡Recuperaste tu territorio 😎!")
+  } else if (vidasJugador == 0){
+    crearMensajefinal("Lo lamento los chetos se quedaron con todo 😭")
+  }
 }
 
 function crearMensaje(resultado){
-    let sectionMensaje = document.getElementById("resultado")
-    let ataquesDelJugador = document.getElementById("ataques-del-jugador")
-    let ataquesDelEnemigo = document.getElementById("ataques-del-enemigo")
+  let sectionMensaje = document.getElementById("resultado");
+  let ataquesDelJugador = document.getElementById("ataques-del-jugador");
+  let ataquesDelEnemigo = document.getElementById("ataques-del-enemigo");
+
+  sectionMensaje.innerHTML = resultado; // Mostrar el resultado del ataque
+
+  let nuevoAtaqueDelJugador = document.createElement("p");
+  let nuevoAtaqueDelEnemigo = document.createElement("p");
+
+  nuevoAtaqueDelJugador.innerHTML = ataqueJugador; 
+  nuevoAtaqueDelEnemigo.innerHTML = ataqueRival;
+
+  ataquesDelJugador.appendChild(nuevoAtaqueDelJugador);
+  ataquesDelEnemigo.appendChild(nuevoAtaqueDelEnemigo);
+}
+
+
+/*function crearMensaje(resultado){
+    let sectionMensaje = document.getElementById("resultado");
+    let ataquesDelJugador = document.getElementById("ataques-del-jugador");
+    let ataquesDelEnemigo = document.getElementById("ataques-del-enemigo");
+
+    sectionMensaje.innerHTML =  resultado;
+    ataquesDelJugador.innerHTML = "ataquesDelJugador";
+    ataquesDelEnemigo.innerHTML = "ataquesDelEnemigo";
 
     let notificacion = document.createElement("p")
     let nuevoAtaqueDelJugador = document.createElement("p")
     let nuevoAtaqueDelEnemigo = document.createElement("p")
 
-    notificacion.innerHTML =  resultado
-    nuevoAtaqueDelJugador.innerHTML = ataquesDelJugador
-    nuevoAtaqueDelEnemigo.innerHTML = ataquesDelEnemigo
-
-
     sectionMensaje.appendChild(notificacion)
     ataquesDelJugador.appendChild(nuevoAtaqueDelJugador)
     ataquesDelEnemigo.appendChild(nuevoAtaqueDelEnemigo)
-}
+}*/
 
 function crearMensajefinal(resultadoFinal){
-    let sectionMensaje = document.getElementById("mensaje")
+    let sectionMensaje = document.getElementById("resultado")
 
     let parrafo = document.createElement("p")
     parrafo.innerHTML = resultadoFinal
